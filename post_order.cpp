bba78b85bc2c30b80489eed17762dda9f3a4a833
M Manzur Morshed <mmorshed@asu.edu>
2017-08-26 10:55:45 -0400

    Update post_order.cpp

--- post_order.cpp
+++ post_order.cpp
@@ -5,7 +5,7 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
-//Solution approach 1
+
 vector<int> Solution::postorderTraversal(TreeNode* A) {
     vector<int> my_return;
   TreeNode* curr_node = A;
@@ -61,52 +61,5 @@ vector<int> Solution::postorderTraversal(TreeNode* A) {
                        }
   }while(!my_stack.empty());
   return my_return;
-
-    
 }
 
-// Solution approach 2 
-vector<int> Solution::postorderTraversal(TreeNode* A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    vector<int> my_return;
-    TreeNode* curr_node = A;
-    stack<TreeNode*> my_stack;
-    TreeNode* temp;
-    if (A == NULL){
-        return my_return;
-    }
-    
-    do{
-        if(curr_node == NULL){
-            temp = my_stack.top();
-            curr_node = temp->right;
-            if(curr_node == NULL){
-                my_return.push_back(temp->val);
-                my_stack.pop();
-                if(my_stack.empty()){
-                    return my_return;
-                                    }
-                curr_node = my_stack.top();
-                while(curr_node->right == temp){
-                    temp = curr_node;
-                    my_return.push_back(curr_node->val);
-                    my_stack.pop();
-                    if(my_stack.empty()){
-                        return my_return;
-                                        }
-                    curr_node = my_stack.top();
-                                   }
-                curr_node = curr_node->right;    
-            }
-        }
-        else{
-            my_stack.push(curr_node);
-            curr_node = curr_node->left;
-            }
-    }while(!my_stack.empty());
-    return my_return;
-    
-}
