28d4d0692a26b7fb20e0648eb94b52bbfcee31cf
M Manzur Morshed <mmorshed@asu.edu>
2018-10-03 13:24:40 -0400

    Update flatten_binary_tree_to_linked_list.py

--- flatten_binary_tree_to_linked_list.py
+++ flatten_binary_tree_to_linked_list.py
@@ -1,3 +1,29 @@
+#Given a binary tree, flatten it to a linked list in-place.
+#
+#Example :
+#Given
+#
+#
+#         1
+#        / \
+#       2   5
+#      / \   \
+#     3   4   6
+#The flattened tree should look like:
+#
+#   1
+#    \
+#     2
+#      \
+#       3
+#        \
+#         4
+#          \
+#           5
+#            \
+#             6
+#Note that the left child of all nodes should be NULL.
+#
 # Definition for a  binary tree node
 # class TreeNode:
 #     def __init__(self, x):
